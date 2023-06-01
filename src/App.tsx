cf030f6e667255f2b79ce56800eb9432a6ea196b
meteor91 <bilalsir@yandex.ru>
2023-06-01 23:27:31 +0600

    added project basic configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,41 @@
-import React, {Suspense, useEffect} from "react";
-import {MFComponent} from "./core/moduleFederation/MFComponent";
-// @ts-ignore
-// const RemoteApp = React.lazy(() => import("app2/App"));
+import React from 'react';
+import { MFComponent } from './core/moduleFederation/MFComponent';
+import { Button } from 'redrock';
 
-// @ts-ignore
-// const RemoteButton = React.lazy(() => import("app2/Button"));
-export const App = () => {
-    // useEffect(() => {
-    //     console.log(RemoteApp);
-    // }, [])
+export const App: React.FC = () => {
     return (
         <div>
             <div>
                 its work kekis
             </div>
             <div>
-            {/*<Suspense fallback={"loading..."}>*/}
-            {/*    <RemoteApp/>*/}
-            {/*</Suspense>*/}
-            <MFComponent
-                url="http://localhost:8080/remoteEntry.js"
-                scope="app2"
-                module='./App'
-            />
-            {/*<Suspense fallback={"loading..."}>*/}
-            {/*    <RemoteButton label="kek" onClick={() => console.log('keknul')}/>*/}
-            {/*</Suspense>*/}
-                <MFComponent<{onClick: () => void; label: string}>
+                <Button label='test' onClick={(_e) => { console.log('kekis'); }}/>
+            </div>
+            <div>
+                {/* <Suspense fallback={"loading..."}> */}
+                {/*    <RemoteApp/> */}
+                {/* </Suspense> */}
+                <MFComponent
+                    url="http://localhost:8080/remoteEntry.js"
+                    scope="app2"
+                    module='./App'
+                />
+                {/* <Suspense fallback={"loading..."}> */}
+                {/*    <RemoteButton label="kek" onClick={() => console.log('keknul')}/> */}
+                {/* </Suspense> */}
+                <MFComponent<{ onClick: () => void, label: string }>
                     url="http://localhost:8080/remoteEntry.js"
                     scope="app2"
                     module='./Button'
                     moduleProps={{
-                        label: "kek", onClick: () => console.log('keknul')
+                        label: 'kek', onClick: () => { console.log('keknul'); },
                     }}
                 />
             </div>
         </div>
-    )
-}
-//@ts-ignore
+    );
+};
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 window.ww = __webpack_share_scopes__;
--- src/core/moduleFederation/hooks/useFederatedComponent.ts
+++ src/core/moduleFederation/hooks/useFederatedComponent.ts
@@ -1,18 +1,26 @@
 import React from 'react';
-import {useDynamicScript} from "./useDynamicScript";
+import { useDynamicScript } from './useDynamicScript';
 
 const componentCache = new Map();
 export const useFederatedComponent = (
     remoteUrl: string,
     scope: string,
-    module: string
-) => {
+    module: string,
+): {
+    failed: boolean
+    Component: null | React.LazyExoticComponent<any>
+} => {
     const key = `${remoteUrl}-${scope}-${module}`;
-    const [Component, setComponent] = React.useState(null);
+    const [
+        Component,
+        setComponent,
+    ] = React.useState<null | React.LazyExoticComponent<any>>(null);
 
-    const {ready, failed} = useDynamicScript(remoteUrl);
+    const { ready, failed } = useDynamicScript(remoteUrl);
     React.useEffect(() => {
-        if (Component) setComponent(null);
+        if (Component) {
+            setComponent(null);
+        }
         // Only recalculate when key changes
     }, [key]);
 
@@ -22,27 +30,44 @@ export const useFederatedComponent = (
             componentCache.set(key, Comp);
             setComponent(Comp);
         }
-        // key includes all dependencies (scope/module)
+    // key includes all dependencies (scope/module)
     }, [Component, ready, key]);
 
-    return {failed, Component};
+    return { failed, Component };
 };
 
-function loadComponent(scope: string, module: string) {
+function loadComponent (scope: string, module: string) {
     return async () => {
-        // Initializes the share scope. This fills it with known provided modules from this build and all remotes
-        //@ts-ignore
-        await __webpack_init_sharing__('default');
+    // Initializes the share scope. This fills it with known provided modules from this build and all remotes
 
-        //@ts-ignore
+        await __webpack_init_sharing__('default');
         const container: Container = (window as any)[scope] as Container; // or get the container somewhere else
         // Initialize the container, it may provide shared modules
-        //@ts-ignore
+
         await container.init(__webpack_share_scopes__.default);
 
         //    const factory = await window[scope].get(module);
         const factory = await container.get(module);
-        const Module = factory();
-        return Module;
+        return factory();
     };
 }
+
+/* Webpack types */
+interface Container {
+    init: (shareScope: any) => Promise<any>
+    get: (module: string) => Promise<any>
+}
+
+/**
+ * Webpack default variable that initializes the shared scope
+ * and adds all the known provided modules from the local build
+ * or the remote container build.
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+declare const __webpack_init_sharing__: (shareScope: string) => Promise<void>;
+
+/**
+ * Default Webpack variable, which initializes the exposed module or the container.
+ */
+// eslint-disable-next-line @typescript-eslint/naming-convention
+declare const __webpack_share_scopes__: { default: any };
